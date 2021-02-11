f727a9477da670f9e2d0fe2af7d55e479f717eee
Pambani Chaqtitantri <78403822+pambanii@users.noreply.github.com>
2021-02-11 09:48:24 +0700

    latihan pemrograman dengan modul

---
+++ coba fibo.py
@@ -0,0 +1,18 @@
+# import mandiri script program induk
+
+#import modul
+
+import fibo
+
+#pemanggilan fungsi pertama
+
+print(fibo.fib(100))
+
+#pemanggilan fungsi ke dua
+
+print(fibo.fib2(100))
+
+#pemanggilan fungsi dengan nama lokal
+
+fib = fibo.fib
+print(fib(100))
